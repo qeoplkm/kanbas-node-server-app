c8d9fef979ecef29ad2d0877d544dcb3252d2e8e
qeoplkm <jeremyforwork121@gmail.com>
2024-11-22 14:23:02 -0500

    a5

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
 import express from 'express'
 import Hello from "./Hello.js"
-import Lab5 from "./Lab5/index.js"
-import cors from "cors"
-import UserRoutes from './Kanbas/Users/routes.js'
-import session from "express-session"
-import "dotenv/config"
-import CourseRoutes from './Kanbas/Courses/routes.js'
-import ModuleRoutes from './Kanbas/Modules/routes.js'
-import AssignmentRoutes from './Kanbas/Assignments/routes.js'
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import UserRoutes from './Kanbas/Users/routes.js';
+import session from "express-session";
+import "dotenv/config";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
-const app = express()
-const PORT = process.env.PORT || 4000
-const HOST = '0.0.0.0'
-
+const port = process.env.PORT || 4000;
+const app = express();
 app.use(
   cors({
     credentials: true,
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
-)
-
+);
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
-}
-
+};
 if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true
+  sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
     domain: process.env.NODE_SERVER_DOMAIN,
-  }
+  };
 }
+app.use(session(sessionOptions));
+app.use(express.json());
 
-app.use(session(sessionOptions))
-app.use(express.json())
+UserRoutes(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+Hello(app);
+Lab5(app);
 
-UserRoutes(app)
-CourseRoutes(app)
-ModuleRoutes(app)
-AssignmentRoutes(app)
-Hello(app)
-Lab5(app)
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
